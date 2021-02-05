e5b9e918aa7c0b8ed2f70a10ac8077541bf60878
t4t5u0 <ymmtryk0902@gmail.com>
2021-02-05 20:26:43 +0900

    [add] ファイルネームを時間として写真を撮ることができる

---
+++ go.mod
@@ -0,0 +1,9 @@
+module go-shutter
+
+go 1.15
+
+require (
+	github.com/BurntSushi/xgb v0.0.0-20210121224620-deaf085860bc // indirect
+	github.com/gen2brain/shm v0.0.0-20200228170931-49f9650110c5 // indirect
+	github.com/kbinani/screenshot v0.0.0-20191211154542-3a185f1ce18f // indirect
+)
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"os"
+	"time"
+
+	"github.com/kbinani/screenshot"
+)
+
+func save(img *image.RGBA, filePath string) {
+	file, err := os.Create(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	png.Encode(file, img)
+}
+
+func main() {
+	n := screenshot.NumActiveDisplays()
+	if n <= 0 {
+		panic("Active display not found")
+	}
+
+	var all image.Rectangle = image.Rect(0, 0, 0, 0)
+
+	for i := 0; i < n; i++ {
+		bounds := screenshot.GetDisplayBounds(i)
+		all = bounds.Union(all)
+
+		img, err := screenshot.CaptureRect(bounds)
+		if err != nil {
+			panic(err)
+		}
+		fileName := time.Now().Format("2006-01-02_15:04:05") + ".png"
+		save(img, fileName)
+		fmt.Printf("#%d : %v \"%s\"\n", i, bounds, fileName)
+	}
+
+	fmt.Printf("%v\n", all)
+	img, err := screenshot.Capture(all.Min.X, all.Min.Y, all.Dx(), all.Dy())
+	if err != nil {
+		panic(err)
+	}
+	fileName := time.Now().Format("2006-01-02_15:04")
+	save(img, fileName)
+}
