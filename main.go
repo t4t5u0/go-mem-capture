b59a94849ca3613b03ed2e759d65f12cae6bb9df
t4t5u0 <ymmtryk0902@gmail.com>
2021-02-12 05:48:19 +0900

    [add] ざっくりとした処理の流れを書いた

--- main.go
+++ main.go
@@ -15,11 +15,15 @@ func main() {
 	// if err != nil {
 	// 	log.Fatal(err)
 	// }
-
+	var (
+		cmd   *exec.Cmd
+		wFlag bool = contains(args, "--widnow") || contains(args, "-w")
+		aFlag bool = contains(args, "--area") || contains(args, "-a")
+		cFlag bool = contains(args, "--copy") || contains(args, "-c")
+	)
 	filepath := "./img/"
 	filename := time.Now().Format("20060102150405")
 	filetype := ".png"
-	var cmd *exec.Cmd
 	fmt.Println(args)
 	if contains(args, "--help") || contains(args, "-h") {
 		return
@@ -27,10 +31,19 @@ func main() {
 	if len(args) == 0 {
 		// デフォルト。スクリーン全体を取得する
 		cmd = exec.Command("maim", filepath+filename+filetype)
-	} else if contains(args, "--window") || contains(args, "-w") {
+	} else if wFlag && aFlag {
+		// このパラメータ指定は不正
+	} else if wFlag {
 		fmt.Println()
 		// アクティブウィンドウをキャプチャ
-		cmd = exec.Command("maim", "-i", "-B", filepath+filename+filetype)
+		// xdotoop getactivewindow で id を持ってくる必要あり
+		cmd = exec.Command("maim", "-B", "-i", filepath+filename+filetype)
+	} else if aFlag {
+
+	}
+
+	if cFlag {
+		// クリップボードにコピー
 	}
 	// cmd := exec.Command("maim", filepath+filename+filetype)
 
@@ -39,12 +52,9 @@ func main() {
 
 }
 
-type param struct {
-	width  int
-	height int
-}
-
 // ffmpeg -f x11grab -i $DISPLAY ./$(date +%s).png
+
+// 配列に要素が含まれているか判定する
 func contains(s []string, e string) bool {
 	for _, v := range s {
 		if e == v {
