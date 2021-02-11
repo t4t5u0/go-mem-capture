be41a164f71d4a50dbe50ca3ef4833413d83fd18
t4t5u0 <ymmtryk0902@gmail.com>
2021-02-12 05:07:25 +0900

    [add] help用のファイルを作成 er the commit message for your changes. Lines starting

--- main.go
+++ main.go
@@ -3,13 +3,24 @@ package main
 import (
 	"os/exec"
 	"time"
+
+	flags "github.com/jessevdk/go-flags"
 )
 
 func main() {
+	// f := flag.
+	flags.Parse(&Option)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// if len(args) == 0 {
+
+	// }
 	filepath := "./img/"
 	filename := time.Now().Format("2015061200171")
 	filetype := ".png"
-	cmd := exec.Command("maim", filepath+filename+filetype)
+	// cmd := exec.Command("maim", filepath+filename+filetype)
+	cmd := exec.Command("maim", "-i", "-B", filepath+filename+filetype)
 	cmd.Start()
 	cmd.Output()
 
